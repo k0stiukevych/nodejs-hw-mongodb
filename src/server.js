6d0331c2a76ee72c80efa8005fa8b6da2c919cba
kostiukevechdima <kostiukevechdima@gmail.com>
2024-10-07 23:51:52 +0300

    commit

--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ import cors from 'cors';
 
 import { env } from './utils/env.js';
 
-import * as contactServices from './services/contacts.js';
+import notFoundHandler from './middlewares/notFoundHandler.js';
+import errorHandler from './middlewares/errorHandler.js';
+
+import contactsRouter from './routers/contacts.js';
 
 export const startServer = () => {
   const app = express();
@@ -11,44 +14,11 @@ export const startServer = () => {
   app.use(cors());
   app.use(express.json());
 
-  app.get('/contacts', async (req, res) => {
-    const data = await contactServices.getAllContacts();
+  app.use('/contacts', contactsRouter);
 
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data,
-    });
-  });
+  app.use(notFoundHandler);
 
-  app.get('/contacts/:ContactId', async (req, res) => {
-    const { ContactId } = req.params;
-    const data = await contactServices.getContactById(ContactId);
-
-    if (!data) {
-      return res.status(404).json({
-        message: `Contact with id=${ContactId} not found`,
-      });
-    }
-
-    res.json({
-      status: 200,
-      message: `Contact with ${ContactId} successfully find`,
-      data,
-    });
-  });
-
-  app.use((req, res) => {
-    res.status(404).json({
-      message: `${req.url} not found`,
-    });
-  });
-
-  app.use((error, req, res, next) => {
-    res.status(500).json({
-      message: error.message,
-    });
-  });
+  app.use(errorHandler);
 
   const port = Number(env('PORT', 3000));
 
